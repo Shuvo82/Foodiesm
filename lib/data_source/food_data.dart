b0dc8110905627d977909cbdab429fce5117d939
Shuvo <sohorafuzzamanshuvo@gmail.com>
2022-08-29 22:57:39 +0600

    data_set ready

---
+++ lib/data_source/food_data.dart
@@ -0,0 +1,19 @@
+class FoodDataSource {
+  static List<Map?>foods = [
+    {
+      "id": 1,
+      "name": "Apple",
+      "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
+      "type":"Fruit",
+      //this information is for per 100 gram of this fruit
+      "vitamin": ["C", "Poison"],
+      "calories": "52",
+      "tests":"Sweet",
+      "calcium":"0%",
+      "details": "An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus",
+
+
+    },
+
+  ];
+}
