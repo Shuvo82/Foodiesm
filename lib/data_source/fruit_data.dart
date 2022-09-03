ede9d5e5a4786fe0d80aafcadc7b7c78cc7ba3c6
Shuvo <sohorafuzzamanshuvo@gmail.com>
2022-09-03 21:16:43 +0600

    fruit image

--- lib/data_source/fruit_data.dart
+++ lib/data_source/fruit_data.dart
@@ -1,49 +1,74 @@
 class FoodDataSource {
-  static List<Map?>foods = [
+  static List<Map?> foods = [
     {
       "id": "1",
       "name": "Apple",
-      "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAMRxq6k47wjlZ34GoJL0_SLH9bfIpLTF4Rd2Dh_yP35IdTPv20UyoWpwlq8Deb13ssdw&usqp=CAU",
-      "type":["Fruit"],
+      "img":
+          "https://lh6.googleusercontent.com/9BsZ53BEI8iBj5t9FsXs6-X7KeLklQwNOxcsNkf94tG6JURr3X3DJ8_tczUGygrW9mw=w2400",
+      "type": ["Fruit"],
       //this information is for per 100 gram of this fruit
       "vitamin": ["C", "B6", "d"],
       "calories": "52",
-      "tests":"Sweet",
-      "calcium":"0%",
-      "details": "An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus",
-
-
+      "tests": "Sweet",
+      "calcium": "0%",
+      "details":
+          "An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus",
     },
     {
       "id": "2",
       "name": "Avocado",
-      "img": "https://img.freepik.com/premium-photo/avocado-with-cut-halfand-green-leaves-isolated_252965-586.jpg",
-      "type":["Fruit"],
+      "img":"",
+      "type": ["Fruit"],
       //this information is for per 100 gram of this fruit
       "vitamin": ["C", "B6", "E", "K"],
       "calories": "240",
-      "tests":"Creamy, and almost buttery",
-      "calcium":"1%",
-      "details": "An avocado is a bright green fruit with a large pit and dark leathery skin. They're also known as alligator pears or butter fruit. Avocados are a favorite of the produce section. They're the go-to ingredient for guacamole dips",
-
-
+      "tests": "Creamy, and almost buttery",
+      "calcium": "1%",
+      "details":
+          "An avocado is a bright green fruit with a large pit and dark leathery skin. They're also known as alligator pears or butter fruit. Avocados are a favorite of the produce section. They're the go-to ingredient for guacamole dips",
     },
     {
       "id": "3",
       "name": "Banana",
-      "img": "https://img.freepik.com/premium-photo/avocado-with-cut-halfand-green-leaves-isolated_252965-586.jpg",
-      "type":["Fruit","Vegetable"],
+      "img": "",
+      "type": ["Fruit", "Vegetable"],
       //this information is for per 100 gram of this fruit
       "vitamin": ["C", "D", "B6"],
       "calories": "89",
-      "tests":"Sweet",
-      "calcium":"0%",
-      "details": "Bananas are one of the most popular fruits worldwide. They contain essential nutrients that can have a protective impact on health.",
-
-
+      "tests": "Sweet",
+      "calcium": "0%",
+      "details":
+          "Bananas are one of the most popular fruits worldwide. They contain essential nutrients that can have a protective impact on health.",
     },
+    {
+      "id": "4",
+      "name": "Guava",
+      "img":
+          "https://lh5.googleusercontent.com/sDvSEtqec0rqiC207HcKYt4YGjuKtzQADQEI_qmV087sjAGM7u-EwioaLVAqEuGkrAA=w2400",
+      "type": ["Fruit", "Vegetable"],
+      //this information is for per 100 gram of this fruit
+      "vitamin": ["C", "D", "B6"],
+      "calories": "89",
+      "tests": "Sweet",
+      "calcium": "0%",
+      "details":
+          "Bananas are one of the most popular fruits worldwide. They contain essential nutrients that can have a protective impact on health.",
+    },
+    {
+      "id": "5",
+      "name": "Mango",
+      "img":
+          "https://lh3.googleusercontent.com/LmHkJR7hUD42X_l_kKF40nZBTcCuD9NFkYEdqLJG9MWNBo5HrCceziS6EIOgj_qm3ik=w2400",
+      "type": ["Fruit", "Vegetable"],
+//this information is for per 100 gram of this fruit
+      "vitamin": ["C", "D", "B6"],
+      "calories": "89",
+      "tests": "Sweet",
+      "calcium": "0%",
+      "details":
+          "Bananas are one of the most popular fruits worldwide. They contain essential nutrients that can have a protective impact on health.",
 
-    //other food info will be added here
-
+//other food info will be added here
+    },
   ];
 }
