9b1dd425450e6d326941d14762ac4d0a43ab6998
s-maliha <smaliha191201@bscse.uiu.ac.bd>
2022-09-08 13:40:28 +0600

    update2

--- lib/data_source/fruit_data.dart
+++ lib/data_source/fruit_data.dart
@@ -3,23 +3,22 @@ class FruitDataSource {
     {
       "id": "1",
       "name": "Apple",
-      "img": "https://lh6.googleusercontent.com/9BsZ53BEI8iBj5t9FsXs6-X7KeLklQwNOxcsNkf94tG6JURr3X3DJ8_tczUGygrW9mw=w2400",
+      "img": "https://lh6.googleusercontent.com/B04jaZddFiKNqTDJ5TDXwSgDOHGh5dAJ9Xw-nMMlqdS1Zj7hKb1sSy7J4clouUEtdvs=w2400",
       "type": ["Fruit"],
-
       //this information is for per 100 gram of this fruit
       "vitamins": ["C", "B6", "d"],
       "calorie": "52",
       "test": "Sweet",
       "calcium": "0%",
       "iron": "0%",
-      "magnesium": "0%",
+      "magnesium": "1%",
       "about":
           "An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus",
     },
     {
       "id": "2",
-      "name": "Mango",
-      "img":"https://lh3.googleusercontent.com/LmHkJR7hUD42X_l_kKF40nZBTcCuD9NFkYEdqLJG9MWNBo5HrCceziS6EIOgj_qm3ik=w2400",
+      "name": "Avocado",
+      "img":"https://lh6.googleusercontent.com/EcAlKvm5NKEeuAfs2stY0bxFrCmWZx0bCCrgtQzVpK5a0JdcRIvY3j_IkrW12HonJYI=w2400",
       "type": ["Fruit"],
       //this information is for per 100 gram of this fruit
       "vitamins": ["C", "B6", "E", "K"],
@@ -29,24 +28,56 @@ class FruitDataSource {
       "iron": "0%",
       "magnesium": "0%",
       "about":
-          "An avocado is a bright green fruit with a large pit and dark leathery skin.  Avocados are a favorite of the produce section. They're the go-to ingredient for guacamole dips",
+      "An avocado is a bright green fruit with a large pit and dark leathery skin.  Avocados are a favorite of the produce section. They're the go-to ingredient for guacamole dips",
     },
     {
       "id": "3",
-      "name": "Guava",
+      "name": "Banana",
       "img":
-          "https://lh5.googleusercontent.com/sDvSEtqec0rqiC207HcKYt4YGjuKtzQADQEI_qmV087sjAGM7u-EwioaLVAqEuGkrAA=w2400",
+          " https://lh5.googleusercontent.com/j1OjLhTY90k4b8lcy5siRPqkPem2L1rUbNXiMNdgYoBYW20s9Flo0MRCwaDr46kzBi0=w2400",
       "type": ["Fruit", "Vegetable"],
       //this information is for per 100 gram of this fruit
       "vitamins": ["C", "D", "B6"],
       "calorie": "89",
       "test": "Sweet",
       "calcium": "0%",
-      "iron": "0%",
-      "magnesium": "0%",
+      "iron": "1%",
+      "magnesium": "6%",
       "about":
           "Bananas are one of the most popular fruits worldwide. They contain essential nutrients that can have a protective impact on health.",
     },
+    {
+      "id": "4",
+      "name": "Cherry",
+      "img":"https://lh3.googleusercontent.com/k7O-8PNqtU3kpoaaLwAiU3QEWRZe7KLllwr0770WWgwXScPbCHyxnuoBrVPE1VUdfP4=w2400",
+      "type": ["Fruit"],
+      //this information is for per 100 gram of this fruit
+      "vitamins": ["C"],
+      "calorie": "50",
+      "test": "sweet and sour",
+      "calcium": "1%",
+      "iron": "1%",
+      "magnesium": "2%",
+      "about":
+      "A cherry is a fruit that grows on a tree or a bush. It belongs to the genus Prunus. It is red in color, with a seed in the middle. It tastes slightly sour, and is often used to flavour cakes and ice cream, or is baked in a pie or cobbler.",
+    },
+
+    // {
+    //   "id": "5",
+    //   "name": "Mango",
+    //   "img":" ",
+    //   "type": ["Fruit"],
+    //   //this information is for per 100 gram of this fruit
+    //   "vitamins": ["C", "B6", "A"],
+    //   "calorie": "65",
+    //   "test": "Sweet",
+    //   "calcium": "1%",
+    //   "iron": "1%",
+    //   "magnesium": "2%",
+    //   "about":
+    //   "A ripe fresh mango can be described as tasting sweet, citrus, and almost melon flavor all in one.",
+    // },
+
 
   ];
 }
